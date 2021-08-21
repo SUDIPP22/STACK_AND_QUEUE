84038c4427c16e38c7f0c5ec42b1bb066c095574
SUDIPP22 <panja370@gmail.com>
2021-08-21 07:45:01 +0530

    Added : Welcome message

---
+++ src/com/bridgelabz/StackQueueMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Purpose - To implement Stack and Queue Operations
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-08-20
+ */
+public class StackQueueMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Program");
+    }
+}
